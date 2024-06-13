7c6d6eb40729c7cfbef9ef227247a794b6cf0de2
hartecho <thomas@hartecho.com>
2024-06-13 14:16:03 -0600

    Adjusted env variables in config file.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,5 +20,18 @@ export default defineNuxtConfig({
   ],
   googleSignIn: {
     clientId: process.env.GOOGLE_CLIENT_ID,
+  },
+  runtimeConfig: {
+    // Private keys are only available server-side
+    JWT_SECRET: process.env.JWT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    
+    // Public keys that are exposed to the client
+    public: {
+      JWT_SECRET: process.env.JWT_SECRET,
+      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    }
   }
 })
