4d8211d2fe41523b575954c8d8ac6dcd12ef4c65
hartecho <thomas@hartecho.com>
2024-06-13 18:38:04 -0600

    Trying to find where the variables are.

--- server/utils/dbConnect.js
+++ server/utils/dbConnect.js
@@ -4,6 +4,10 @@ const config = useRuntimeConfig();
 const MONGODB_URI = config.DB_URI; // Ensure your DB URI is correctly set in your environment
 
 export const connectDB = async () => {
+  console.log("config.DB_URI: ", config.DB_URI);
+  console.log("config.public.DB_URI: ", config.public.DB_URI);
+  console.log("config.private.DB_URI: ", config.private.DB_URI);
+  console.log("process.env: ", process.env);
   if (mongoose.connection.readyState === 1) {
     console.log('Database already connected');
     return;
