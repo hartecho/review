65bdcfcb0fcfbbfb3ba2261aa0916d24e09ad202
hartecho <thomas@hartecho.com>
2024-08-10 13:29:18 -0600

    Console.log statements to server for contractor get API.

--- server/api/contractors/index.get.js
+++ server/api/contractors/index.get.js
@@ -7,17 +7,24 @@ export default defineEventHandler(async (event) => {
   const query = getQuery(event);
   const _id = query._id;
 
+  console.log("QUERY FOR CONTRACTOR GET API" + query);
+
   try {
     if (_id) {
+      console.log("FOUND _ID IN API");
       const contractor = await Contractor.findById(_id);
+      console.log("SINGLE CONTRACTOR FOUND: " + contractor);
       await disconnectDB(); // Disconnect from DB for static file generation to close after build success
       return contractor;
     } else {
+      console.log("FETCHING ALL CONTRACTORS");
       const allContractors = await Contractor.find({});
+      console.log("CONTRACTORS: " + allContractors);
       await disconnectDB();
       return allContractors;
     }
   } catch (error) {
+    console.log("WAS ERROR FETCHING CONTRACTORS: " + error.message);
     console.error(error);
     throw createError({ statusCode: 500, message: 'Server Error' });
   }
